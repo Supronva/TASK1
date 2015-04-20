3ad08b7943c5e26ad6631bfa13ec8c2e63fa8492
Supronva <oag4400@gmail.com>
2015-04-20 23:53:34 +0300

    Task1

---
+++ Epam-task-1/LinearFuction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam_task_1
+{
+   public class LinearFuction
+    {
+       private double a,b;
+
+       public LinearFuction()
+       { 
+       }
+
+       public LinearFuction(double a,double b)
+   {
+           this.a = a;
+           this.b = b;
+
+   }
+
+       public double A
+       {
+           get { return a; }
+       }
+
+       public double B
+       {
+           get { return b; }
+       }
+
+       public double Fuction(int x)
+       {
+           return a*x+b;
+       }
+
+     
+    }
+}
---
+++ Epam-task-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            LinearFuction f = new LinearFuction(3, 3);
+            int x = 3;
+            Console.WriteLine(" a = {0}\n b = {1}\n x = {2}\n y=ax+b = {3}",f.A,f.B,x,f.Fuction(x));
+            Console.ReadKey();
+
+        }
+    }
+}
